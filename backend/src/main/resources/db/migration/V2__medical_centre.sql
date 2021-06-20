7444dcbae32add7719f7be1f449b1393d8d7f803
Petr Sykora <petr.sykora@gmail.com>
2021-06-20 10:46:53 +0200

    Add medical centre migration

---
+++ backend/src/main/resources/db/migration/V2__medical_centre.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `medical_centre`
+(
+    `id`       bigint       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name`     varchar(255) NOT NULL,
+    `address`  varchar(255) NOT NULL,
+    `username` varchar(255) NOT NULL
+);
